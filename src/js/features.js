d84772f485c552c0f4001b85365f86186f51204c
Lucas Ferreira Nogueira <lucas.ferreiranogueira@outlook.com>
2024-05-02 10:14:49 -0300

    FIX: resolução do bug do button de switch do tema

--- src/js/features.js
+++ src/js/features.js
@@ -1,38 +1,37 @@
 
 document.addEventListener('DOMContentLoaded', () => {
 
+    // Toggle theme dark/light
     let button = document.getElementById('button-theme');
 
-    console.log(button);
+    let tagHtml = document.getElementsByTagName('html')[0];
+
+    tagHtml.classList.add('theme-light');
+    button.innerHTML = `<span class="icon" style="color: rgb(226, 226, 56);"><i class="fas fa-lg fa-sun"></i></span>`;
 
     button.addEventListener('click', () => {
-        // Toggle theme dark/light
-        let tagHtml = document.getElementsByTagName('html')[0];
-    
-        let isDark = tagHtml.classList.toggle('theme-dark');
-    
+        tagHtml.classList.toggle('theme-light')
+        
+        let isDark = tagHtml.classList.toggle('theme-dark')
+        
         if (isDark) {
-            button.innerHTML = `<span class="icon" style="color: #8156F5;"><i class="fas fa-lg fa-moon"></i></span>`;
+          button.innerHTML = `<span class="icon" style="color: #8156F5;"><i class="fas fa-lg fa-moon"></i></span>`;
         } else {
-            button.innerHTML = `<span class="icon" style="color: rgb(226, 226, 56);"><i class="fas fa-lg fa-sun"></i></span>`;
+          button.innerHTML = `<span class="icon" style="color: rgb(226, 226, 56);"><i class="fas fa-lg fa-sun"></i></span>`;
         }
     })
 
     
 
-    // Dando comportamento ao menu hamburguer
-    // Get all "navbar-burger" elements
+    // Dando comportamento de expansão ao menu hamburguer
     const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
   
-    // Add a click event on each of them
     $navbarBurgers.forEach( el => {
       el.addEventListener('click', () => {
   
-        // Get the target from the "data-target" attribute
         const target = el.dataset.target;
         const $target = document.getElementById(target);
   
-        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
         el.classList.toggle('is-active');
         $target.classList.toggle('is-active');
   
